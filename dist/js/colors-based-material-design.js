a40221912dc3ccda29f95d55fc102b4e9b99c614
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2019-05-17 16:43:44 -0300

    added js with the colors

---
+++ dist/js/colors-based-material-design.js
@@ -0,0 +1,355 @@
+//JSON com as cores do material design!
+export const colors = {
+  colorRed: {
+    darknessColor900: '#b71c1c',
+    darknessColor800: '#c62828',
+    darknessColor700: '#d32f2f',
+    darknessColor600: '#e53935',
+    mainColor: '#f44336',
+    lightnessColor400: '#ef5350',
+    lightnessColor300: '#e57373',
+    lightnessColor200: '#ef9a9a',
+    lightnessColor100: '#ffcdd2',
+    lightnessColor50: '#ffebee',
+  },
+  colorPink: {
+    darknessColor900: '#880E4F',
+    darknessColor800: '#AD1457',
+    darknessColor700: '#C2185B',
+    darknessColor600: '#D81B60',
+    mainColor: '#E91E63',
+    lightnessColor400: '#EC407A',
+    lightnessColor300: '#F06292',
+    lightnessColor200: '#F48FB1',
+    lightnessColor100: '#F8BBD0',
+    lightnessColor50: '#FCE4EC',
+  },
+  colorPurple: {
+    darknessColor900: '#4A148C',
+    darknessColor800: '#6A1B9A',
+    darknessColor700: '#7B1FA2',
+    darknessColor600: '#8E24AA',
+    mainColor: '#9C27B0',
+    lightnessColor400: '#AB47BC',
+    lightnessColor300: '#BA68C8',
+    lightnessColor200: '#CE93D8',
+    lightnessColor100: '#E1BEE7',
+    lightnessColor50: '#F3E5F5',
+  },
+  colorDeepPurple: {
+    darknessColor900: '#311B92',
+    darknessColor800: '#4527A0',
+    darknessColor700: '#512DA8',
+    darknessColor600: '#5E35B1',
+    mainColor: '#673AB7',
+    lightnessColor400: '#7E57C2',
+    lightnessColor300: '#9575CD',
+    lightnessColor200: '#B39DDB',
+    lightnessColor100: '#D1C4E9',
+    lightnessColor50: '#EDE7F6',
+  },
+  colorIndigo: {
+    darknessColor900: '#1A237E',
+    darknessColor800: '#283593',
+    darknessColor700: '#303F9F',
+    darknessColor600: '#3949AB',
+    mainColor: '#3F51B5',
+    lightnessColor400: '#5C6BC0',
+    lightnessColor300: '#7986CB',
+    lightnessColor200: '#9FA8DA',
+    lightnessColor100: '#C5CAE9',
+    lightnessColor50: '#E8EAF6',
+  },
+  colorBlue: {
+    darknessColor900: '#0D47A1',
+    darknessColor800: '#1565C0',
+    darknessColor700: '#1976D2',
+    darknessColor600: '#1E88E5',
+    mainColor: '#2196F3',
+    lightnessColor400: '#42A5F5',
+    lightnessColor300: '#64B5F6',
+    lightnessColor200: '#90CAF9',
+    lightnessColor100: '#BBDEFB',
+    lightnessColor50: '#E3F2FD',
+  },
+  colorLightBlue: {
+    darknessColor900: '#01579B',
+    darknessColor800: '#0277BD',
+    darknessColor700: '#0288D1',
+    darknessColor600: '#039BE5',
+    mainColor: '#03A9F4',
+    lightnessColor400: '#29B6F6',
+    lightnessColor300: '#4FC3F7',
+    lightnessColor200: '#81D4FA',
+    lightnessColor100: '#B3E5FC',
+    lightnessColor50: '#E1F5FE',
+  },
+  colorCyan: {
+    darknessColor900: '#006064',
+    darknessColor800: '#00838F',
+    darknessColor700: '#0097A7',
+    darknessColor600: '#00ACC1',
+    mainColor: '#00BCD4',
+    lightnessColor400: '#26C6DA',
+    lightnessColor300: '#4DD0E1',
+    lightnessColor200: '#80DEEA',
+    lightnessColor100: '#B2EBF2',
+    lightnessColor50: '#E0F7FA',
+  },
+  colorTeal: {
+    darknessColor900: '#004D40',
+    darknessColor800: '#00695C',
+    darknessColor700: '#00796B',
+    darknessColor600: '#00897B',
+    mainColor: '#009688',
+    lightnessColor400: '#26A69A',
+    lightnessColor300: '#4DB6AC',
+    lightnessColor200: '#80CBC4',
+    lightnessColor100: '#B2DFDB',
+    lightnessColor50: '#E0F2F1',
+  },
+  colorGreen: {
+    darknessColor900: '#1B5E20',
+    darknessColor800: '#2E7D32',
+    darknessColor700: '#388E3C',
+    darknessColor600: '#43A047',
+    mainColor: '#4CAF50',
+    lightnessColor400: '#66BB6A',
+    lightnessColor300: '#81C784',
+    lightnessColor200: '#A5D6A7',
+    lightnessColor100: '#C8E6C9',
+    lightnessColor50: '#E8F5E9',
+  },
+  colorLightGreen: {
+    darknessColor900: '#33691E',
+    darknessColor800: '#558B2F',
+    darknessColor700: '#689F38',
+    darknessColor600: '#7CB342',
+    mainColor: '#8BC34A',
+    lightnessColor400: '#9CCC65',
+    lightnessColor300: '#AED581',
+    lightnessColor200: '#C5E1A5',
+    lightnessColor100: '#DCEDC8',
+    lightnessColor50: '#F1F8E9',
+  },
+  colorLime: {
+    darknessColor900: '#827717',
+    darknessColor800: '#9E9D24',
+    darknessColor700: '#AFB42B',
+    darknessColor600: '#C0CA33',
+    mainColor: '#CDDC39',
+    lightnessColor400: '#D4E157',
+    lightnessColor300: '#DCE775',
+    lightnessColor200: '#E6EE9C',
+    lightnessColor100: '#F0F4C3',
+    lightnessColor50: '#F9FBE7',
+  },
+  colorYellow: {
+    darknessColor900: '#F57F17',
+    darknessColor800: '#F9A825',
+    darknessColor700: '#FBC02D',
+    darknessColor600: '#FDD835',
+    mainColor: '#FFEB3B',
+    lightnessColor400: '#FFEE58',
+    lightnessColor300: '#FFF176',
+    lightnessColor200: '#FFF59D',
+    lightnessColor100: '#FFF9C4',
+    lightnessColor50: '#FFFDE7',
+  },
+  colorAmber: {
+    darknessColor900: '#FF6F00',
+    darknessColor800: '#FF8F00',
+    darknessColor700: '#FFA000',
+    darknessColor600: '#FFB300',
+    mainColor: '#FFC107',
+    lightnessColor400: '#FFCA28',
+    lightnessColor300: '#FFD54F',
+    lightnessColor200: '#FFE082',
+    lightnessColor100: '#FFECB3',
+    lightnessColor50: '#FFF8E1',
+  },
+  colorOrange: {
+    darknessColor900: '#E65100',
+    darknessColor800: '#EF6C00',
+    darknessColor700: '#F57C00',
+    darknessColor600: '#FB8C00',
+    mainColor: '#FF9800',
+    lightnessColor400: '#FFA726',
+    lightnessColor300: '#FFB74D',
+    lightnessColor200: '#FFCC80',
+    lightnessColor100: '#FFE0B2',
+    lightnessColor50: '#FFF3E0',
+  },
+  colorDeepOrange: {
+    darknessColor900: '#BF360C',
+    darknessColor800: '#D84315',
+    darknessColor700: '#E64A19',
+    darknessColor600: '#F4511E',
+    mainColor: '#FF5722',
+    lightnessColor400: '#FF7043',
+    lightnessColor300: '#FF8A65',
+    lightnessColor200: '#FFAB91',
+    lightnessColor100: '#FFCCBC',
+    lightnessColor50: '#FBE9E7',
+  },
+  colorBrown: {
+    darknessColor900: '#3E2723',
+    darknessColor800: '#4E342E',
+    darknessColor700: '#5D4037',
+    darknessColor600: '#6D4C41',
+    mainColor: '#795548',
+    lightnessColor400: '#8D6E63',
+    lightnessColor300: '#A1887F',
+    lightnessColor200: '#BCAAA4',
+    lightnessColor100: '#D7CCC8',
+    lightnessColor50: '#EFEBE9',
+  },
+  colorGrey: {
+    darknessColor900: '#212121',
+    darknessColor800: '#424242',
+    darknessColor700: '#616161',
+    darknessColor600: '#757575',
+    mainColor: '#9E9E9E',
+    lightnessColor400: '#BDBDBD',
+    lightnessColor300: '#E0E0E0',
+    lightnessColor200: '#EEEEEE',
+    lightnessColor100: '#F5F5F5',
+    lightnessColor50: '#FAFAFA',
+  },
+  colorBlueGrey: {
+    darknessColor900: '#263238',
+    darknessColor800: '#37474F',
+    darknessColor700: '#455A64',
+    darknessColor600: '#546E7A',
+    mainColor: '#607D8B',
+    lightnessColor400: '#78909C',
+    lightnessColor300: '#90A4AE',
+    lightnessColor200: '#B0BEC5',
+    lightnessColor100: '#CFD8DC',
+    lightnessColor50: '#ECEFF1',
+  },
+  colorBlack: '#000',
+  colorWhite: '#fff',
+  facebookColor: '#3b5999',
+  googleColor: '#dd4b39',
+  flixColor: '#602fe9',
+}
+
+//facilita a pesquisa da cor e tonalidade(tonalidades escuras: 600,700,800,900; tonalidades claras: 50,100,200,300,400)
+export default function Cor(color, colorTonality = 500) {
+  switch (color) {
+    case 'red':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorRed)
+        : colors.colorRed.mainColor
+    case 'blue':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorBlue)
+        : colors.colorBlue.mainColor
+    case 'grey':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorGrey)
+        : colors.colorGrey.mainColor
+    case 'green':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorGreen)
+        : colors.colorGreen.mainColor
+    case 'pink':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorPink)
+        : colors.colorPink.mainColor
+    case 'purple':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorPurple)
+        : colors.colorPurple.mainColor
+    case 'deep-purple':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorDeepPurple)
+        : colors.colorDeepPurple.mainColor
+    case 'indigo':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorIndigo)
+        : colors.colorIndigo.mainColor
+    case 'light-blue':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorLightBlue)
+        : colors.colorLightBlue.mainColor
+    case 'cyan':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorCyan)
+        : colors.colorCyan.mainColor
+    case 'teal':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorTeal)
+        : colors.colorTeal.mainColor
+    case 'light-green':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorLightGreen)
+        : colors.colorLightGreen.mainColor
+    case 'lime':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorLime)
+        : colors.colorLime.mainColor
+    case 'yellow':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorYellow)
+        : colors.colorYellow.mainColor
+    case 'amber':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorAmber)
+        : colors.colorAmber.mainColor
+    case 'orange':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorOrange)
+        : colors.colorOrange.mainColor
+    case 'deep-orange':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorDeepOrange)
+        : colors.colorDeepOrange.mainColor
+    case 'brown':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorBrown)
+        : colors.colorBrown.mainColor
+    case 'blue-grey':
+      return colorTonality >= 50 && colorTonality != 500
+        ? tonality(colorTonality, colors.colorBlueGrey)
+        : colors.colorBlueGrey.mainColor
+    case 'facebook':
+      return colors.facebookColor
+    case 'google':
+      return colors.googleColor
+    case 'flix':
+      return colors.flixColor
+    case 'black':
+      return colors.colorBlack
+    case 'white':
+      return colors.colorWhite
+    default:
+      return console.log('Digite uma cor válida!')
+  }
+}
+
+function tonality(colorTonality, color) {
+  switch (colorTonality) {
+    case 900:
+      return color.darknessColor900
+    case 800:
+      return color.darknessColor800
+    case 700:
+      return color.darknessColor700
+    case 600:
+      return color.darknessColor600
+    case 400:
+      return color.lightnessColor400
+    case 300:
+      return color.lightnessColor300
+    case 200:
+      return color.lightnessColor200
+    case 100:
+      return color.lightnessColor100
+    case 50:
+      return color.lightnessColor50
+    default:
+      return color.mainColor
+  }
+}
